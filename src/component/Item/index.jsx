574c8813a70d924ae43f0b28705f741bd79d97d7
hi25779 <859028027@qq.com>
2022-11-01 22:28:50 +0800

    add mouse action

--- src/component/Item/index.jsx
+++ src/component/Item/index.jsx
@@ -3,15 +3,25 @@ import "./index.css"
 
 export default class Item extends React.Component {
 
+  state = {
+    mouse: false
+  }
+  handleMouse = (flag)=>{
+    return() => {
+      this.setState({mouse:flag})
+    }
+  }
+
   render() {
     const {item} = this.props
+    const {mouse} = this.state
     return(
-      <li>
+      <li style={{background: mouse ? '#ddd' : 'white'}} defaultChecked={item.isDone} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
         <label>
-          <input type={"checkbox"} defaultChecked={item.isDone}/>
+          <input type={"checkbox"} />
           <span>{item.name}</span>
         </label>
-        <button className="btn btn-danger">delete</button>
+        <button className="btn btn-danger" style={{display: mouse ? 'block' :'none'}}>delete</button>
       </li>
     )
   }
